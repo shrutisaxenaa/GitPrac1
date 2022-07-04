9a83fbcf2865a398dfc83ac34ab4baf8a698f0f1
shrutisaxenaa <106474924+shrutisaxenaa@users.noreply.github.com>
2022-07-04 08:37:27 -0500

    secondPush

--- src/main/java/gitPrac1/GitPrac1.java
+++ src/main/java/gitPrac1/GitPrac1.java
@@ -4,6 +4,8 @@ public class GitPrac1 {
 	public static void main(String[] args) {
 		
 		System.out.println("my name is shruti");
+		System.out.println("this is my first project with GIT");
+		System.out.println("i am really happy");
 	}
 
 }
